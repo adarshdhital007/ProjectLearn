2465861901bf0a50085386cff902a234ee3bbd51
Adarsh Dhital Khatri <adarsh7466@gmail.com>
2023-07-27 19:23:17 +0530

    Initial commit

---
+++ PrefixSum/BinarySearch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static int BinarySearch(int[] arr, int target)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        // If the target is not found, return -1
+        return -1;
+    }
+
+    static void Main()
+    {
+        int[] arr = { 1, 3, 5, 7, 9, 11, 13, 15 };
+        int target = 9;
+
+        int index = BinarySearch(arr, target);
+
+        if (index != -1)
+            Console.WriteLine($"Target {target} found at index {index}");
+        else
+            Console.WriteLine($"Target {target} not found in the array.");
+    }
+}
---
+++ PrefixSum/PrefixSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 3, 1, 7, 2, 5, 4 };
+        int n = arr.Length;
+        int[] prefixSum = new int[n];
+
+        // Calculate the prefix sum array
+        prefixSum[0] = arr[0];
+        for (int i = 1; i < n; i++)
+        {
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
+        }
+
+        // Print the prefix sum array
+        Console.WriteLine("Prefix sum array:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(prefixSum[i] + " ");
+        }
+        Console.WriteLine();
+
+        // Find the sum of elements in a specific range [startIndex, endIndex]
+        int startIndex = 1;
+        int endIndex = 4;
+        int sumInRange = prefixSum[endIndex] - (startIndex > 0 ? prefixSum[startIndex - 1] : 0);
+        Console.WriteLine($"Sum in range [{startIndex}, {endIndex}] is: {sumInRange}");
+    }
+}
